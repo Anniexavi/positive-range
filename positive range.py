0aea67f6a05dd0530c8500d6f1b5e83f68fb7da8
Anniexavi <89799680+Anniexavi@users.noreply.github.com>
2021-08-31 00:39:08 +0530

    created positive range.py

---
+++ positive range.py
@@ -0,0 +1,9 @@
+list 1 = [12,-7,5,64,-14]
+for num in list 1:
+   if num >= 0:
+      print (num,end = "")
+      
+list 2 = [12,14,-95,3]
+for num in list 2:
+   if num >= 0:
+      print (num,end = "")
